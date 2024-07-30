732f7257efaa3ae34eb646889c7b150b72dc958b
ih <irvin.hwang@gmail.com>
2024-07-30 12:28:42 -0700

    Use camera for observations in JetbotEnv

--- jetbot/jetbotenv.py
+++ jetbot/jetbotenv.py
@@ -33,6 +33,8 @@
 """Rest everything follows."""
 from collections.abc import Sequence
 import torch
+import gymnasium as gym
+import numpy as np
 from omni.isaac.lab.envs import DirectRLEnv, DirectRLEnvCfg
 import omni.isaac.core.utils.prims as prim_utils
 from omni.isaac.lab.sim import SimulationCfg, SimulationContext
@@ -65,11 +67,13 @@ class JetbotEnvCfg(DirectRLEnvCfg):
     decimation = 2
     episode_length_s = 5.0
     num_actions = 2
-    num_observations = 1 
     action_scale = 100.0
 
     # Scene
     scene: InteractiveSceneCfg = JetbotSceneCfg(num_envs=1, env_spacing=2.0)
+
+    num_channels = 3
+    num_observations = num_channels * scene.camera.height * scene.camera.width
 
 class JetbotEnv(DirectRLEnv):
     cfg: JetbotEnvCfg
@@ -77,17 +81,43 @@ class JetbotEnv(DirectRLEnv):
     def __init__(self, cfg: JetbotEnvCfg, render_mode: str | None = None, **kwargs):
         super().__init__(cfg, render_mode, **kwargs)
         self.robot = self.scene["jetbot"]
+        self.robot_camera = self.scene["camera"]
         self.action_scale = self.cfg.action_scale
+
+    def _configure_gym_env_spaces(self):
+        self.num_actions = self.cfg.num_actions
+        self.num_observations = self.cfg.num_observations
+
+        self.single_observation_space = gym.spaces.Dict()
+        self.single_observation_space["policy"] = gym.spaces.Box(
+            low=-np.inf,
+            high=np.inf,
+            shape=(self.cfg.scene.camera.height, self.cfg.scene.camera.width, self.cfg.num_channels),
+        )
+        self.single_action_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(self.num_actions,))
+
+        # batch the spaces for vectorized environments
+        self.observation_space = gym.vector.utils.batch_space(self.single_observation_space, self.num_envs)
+        self.action_space = gym.vector.utils.batch_space(self.single_action_space, self.num_envs)
+
+        # RL specifics
+        self.actions = torch.zeros(self.num_envs, self.num_actions, device=self.sim.device)
+
 
     def _get_rewards(self) -> torch.Tensor:
         return torch.tensor([1])
     
     def _get_observations(self) -> dict:
-        return {"policy": torch.tensor([1])}
+        observations =  self.robot_camera.data.output["rgb"].clone()
+        print("----Observation----")
+        print(observations)
+        return {"policy": observations}
 
         obs = self.scene
         observations = {"policy": obs}
         return observations
+
+
 
     def _pre_physics_step(self, actions: torch.Tensor) -> None:
         self.actions = self.action_scale * actions.clone()
