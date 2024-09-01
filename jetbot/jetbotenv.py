f601e9a1a5fc55bc9b8b34138a455c319d22e391
ih <irvin.hwang@gmail.com>
2024-09-01 14:42:11 -0700

    Update copyright header for jetbotenv.py

--- jetbot/jetbotenv.py
+++ jetbot/jetbotenv.py
@@ -1,6 +1,9 @@
+# Original Code:
 # Copyright (c) 2022-2024, The Isaac Lab Project Developers.
 # All rights reserved.
 #
+# Modifications:
+# Copyright (c) 2024, Irvin Hwang
 # SPDX-License-Identifier: BSD-3-Clause
 
 """This script demonstrates how to create a simple stage in Isaac Sim.
